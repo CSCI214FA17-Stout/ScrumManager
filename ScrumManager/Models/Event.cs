6c49c9c973ae24fbee30072823f22426bad5b439
John Hardy <hardy8840@gmail.com>
2017-10-08 15:36:25 -0400

    start of event models

---
+++ ScrumManager/Models/Event.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumManager.Models
+{
+    public class Event
+    {
+        //event componenets
+        string eventName;
+        string eventType;
+        string eventLocation;
+        DateTime startTime;
+        DateTime endTime;
+        list<Person> invitees;
+        list<Person> attendees;
+        string Notes;
+
+        //constructor
+        Event(string eventName, string eventType, string eventLocation, DateTime startTime, DateTime endTime)
+        {
+            this.eventName = eventName;
+            this.eventType = eventType;
+            this.eventLocation = eventLocation;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.invitees = new list<Person>();
+            this.attendees = new list<Person>();
+            this.Notes = "";
+        }
+
+    }
+}
---
+++ ScrumManager/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumManager.Models
+{
+    public class Person
+    {
+        private string FirstName;
+        private string LastName;
+    }
+}
