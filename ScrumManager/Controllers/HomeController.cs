8bf57819512c2fc0846b3bb0938a05b590085c07
John Hardy <hardy8840@gmail.com>
2017-10-11 16:37:45 -0400

    changed stuff

--- ScrumManager/Controllers/HomeController.cs
+++ ScrumManager/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ScrumManager.Models;
 
 namespace ScrumManager.Controllers
 {
@@ -31,7 +29,7 @@ namespace ScrumManager.Controllers
 
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
