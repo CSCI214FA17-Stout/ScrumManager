26c18dc6488bf3df3b9a5c89a645884918c61a65
holleyic <31743298+holleyic@users.noreply.github.com>
2017-11-05 16:53:20 -0500

    Added/Modified Entities

--- ScrumManager/Models/Entities/Event.cs
+++ ScrumManager/Models/Entities/Event.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +8,7 @@ namespace ScrumManager.Models
     public class Event
     {
         //event componenets
-        public int ID { get; set; }
+        public int ID { get; private set; }
         public string EventName { get; set;}
         public string EventType { get; set; }
         public string EventLocation { get; set; }
@@ -19,12 +18,21 @@ namespace ScrumManager.Models
         public List<Person> Attendees { get; set; }
         public string Notes { get; set; }
 
-        //constructor
+        //default constructor
         public Event()
         {
+            this.ID = -1;
             this.Invitees = new List<Person>();
             this.Attendees = new List<Person>();
+            this.Notes = "";
         }
 
+        public Event(int ID)
+        {
+            this.ID = ID;
+            this.Invitees = new List<Person>();
+            this.Attendees = new List<Person>();
+            this.Notes = "";
+        }
     }
 }
--- ScrumManager/Models/Entities/Person.cs
+++ ScrumManager/Models/Entities/Person.cs
@@ -7,18 +7,35 @@ namespace ScrumManager.Models
 {
     public class Person
     {
-        private string FirstName;
-        private string LastName;
+        public int ID { get; private set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        public List<Task> Tasks { get; set; }
 
-        public Person(string firstname, string lastname)
+        public Person(int ID, string firstname, string lastname)
         {
+            this.ID = ID;
             this.FirstName = firstname;
             this.LastName = lastname;
         }
 
-        public string ToString()
+        public override string ToString()
         {
             return FirstName + " " + LastName;
+        }
+
+        public void AddTask(Task t)
+        {
+            Tasks.Add(t);
+        }
+
+        /// <summary>
+        /// Removes a task.
+        /// </summary>
+        /// <returns>False if the task was not found. True otherwise.</returns>
+        public bool RemoveTask(Task t)
+        {
+            return Tasks.Remove(t);
         }
     }
 }
